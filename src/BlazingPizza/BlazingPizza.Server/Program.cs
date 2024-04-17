d2e5d27658c9aad4154fe8d71a602a46309696d9
Tomáš Herceg <tomas.herceg@riganti.cz>
2024-04-17 18:17:21 +0200

    Tiny fixes

--- src/BlazingPizza/BlazingPizza.Server/Program.cs
+++ src/BlazingPizza/BlazingPizza.Server/Program.cs
@@ -11,10 +11,13 @@ using Microsoft.Extensions.Hosting;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddMvc();
+builder.Services.AddControllers();
 builder.Services.AddDbContext<PizzaStoreContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DB"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DB"), options =>
+    {
+        options.EnableRetryOnFailure();
+});
 });
 builder.Services.AddResponseCompression(options =>
 {
@@ -35,14 +38,11 @@ app.UseAuthorization();
 app.MapControllers();
 
 // Run database migrations
-var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
-using (var scope = scopeFactory.CreateScope())
-{
+using var scope = app.Services.CreateScope();
     var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
     if (db.Database.EnsureCreated())
     {
         SeedData.Initialize(db);
     }
-}
 
 app.Run();
