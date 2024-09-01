65258ab55cf3e493b7dc50fd6af439d818b27979
Tomáš Herceg <tomas.herceg@riganti.cz>
2024-09-01 19:17:22 +0200

    Deployment

--- src/BlazingPizza/BlazingPizza.AppHost/Program.cs
+++ src/BlazingPizza/BlazingPizza.AppHost/Program.cs
@@ -2,12 +2,14 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var db = builder.AddSqlServer("blazingpizza-db")
     .WithDataVolume()
+    .PublishAsAzureSqlDatabase()
     .AddDatabase("DB", "BlazingPizza");
 
 var server = builder.AddProject<Projects.BlazingPizza_Server>("blazingpizza-server")
     .WithReference(db);
 
 var app = builder.AddProject<Projects.BlazingPizza_App>("blazingpizza-app")
+    .WithExternalHttpEndpoints()
     .WithReference(server);
 
 builder.Build().Run();
